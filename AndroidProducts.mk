37ff2dbe957d59f021a9f6843db8ba4d76a13239
SekaiMoe <pdvpuyvbbs@hotmail.com>
2024-01-03 12:08:20 +0800

    PCRM00: Initial Push

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_pcrm00.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_pcrm00-userdebug \
+    aosp_pcrm00-eng
